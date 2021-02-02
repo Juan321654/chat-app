b87c9997e5bf94a89c06832e688b81a8a73c8ede
Juan Camacho <kmilokmacho@hotmail.com>
2021-02-02 18:27:21 -0500

    added cors with options

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http)
+const app = require('express')()
+const options = {
+  cors:true,
+  origins:["http://127.0.0.1:5347"],
+ }
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, options)
+const cors = require('cors');
+app.use(cors())
 
 io.on('connection', socket => {
-  socket.on('message', ({name, message}) => {
-    io.emit('message', {name, message});
+  socket.on('message', ({ name, message }) => {
+    io.emit('message', { name, message })
   })
 })
 
-http.listen(4000, function () {
-  console.log('listening on port 4000');
+http.listen(4000, function() {
+  console.log('listening on port 4000')
 })
